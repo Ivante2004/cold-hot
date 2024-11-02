6313edac09f4b7b3090638a6f195ae8a9690a859
Ivante2004 <tsybisov2004@mail.ru>
2024-11-02 22:10:19 +0300

    Task04

--- src/Controller.php
+++ src/Controller.php
@@ -2,19 +2,151 @@
 
 namespace Ivante2004\ColdHot\Controller;
 
-use Ivante2004\ColdHot\View;
-use Ivante2004\ColdHot\Game;
+use Ivante2004\ColdHot\View\View;
+use Ivante2004\ColdHot\Model\Model;
 
-function startGame() {
-    View\showStartScreen();
-    
-    $game = new Game();
+class Controller
+{
+    public function generateSecretNumber()
+    {
+        $arrayNumbers = [];
+        $length = 3;
 
-    do {
-        $guess = View\getUserInput();
-        $feedback = $game->checkGuess((int) $guess);
-        View\showFeedback($feedback);
-    } while (!$game->isCorrectGuess((int) $guess));
+        while (count($arrayNumbers) !== $length) {
+            $number = rand(1, 9);
 
-    View\showFeedback("Congratulations! You've guessed the number in " . $game->getAttempts() . " attempts.");
+            if (!in_array($number, $arrayNumbers)) {
+                $arrayNumbers[] = $number;
+            }
+        }
+
+        $result = implode("", $arrayNumbers);
+
+        return $result;
+    }
+
+    public function menu()
+    {
+
+        $isExist = file_exists('cold-hot.db');
+
+        View::showGame($isExist);
+
+        $choiceUser = readline("Выберите действие: ");
+
+        switch ($choiceUser) {
+            case 1:
+                $this->startGame();
+                break;
+            case 2:
+                if ($isExist) {
+                    $this->showHistory();
+                    break;
+                } else {
+                    echo "Таблицы не существует";
+                    break;
+                }
+            case 3:
+                if ($isExist) {
+                    $this->showGameRepeat();
+                    break;
+                } else {
+                    echo "Таблицы не существует";
+                    break;
+                }
+            default:
+                echo "Неверный выбор";
+                break;
+        }
+    }
+
+    public function startGame()
+    {
+        $isFinished = false;
+        $counter = 0;
+        $hints = [];
+        $triesCounter = 0;
+
+        $username = readline("Ваше имя: ");
+
+        if (! file_exists('cold-hot.db')) {
+            $model = new Model('cold-hot.db');
+            $model->createTables();
+        } else {
+            $model = new Model('cold-hot.db');
+        }
+
+        $secretNumber = $this->generateSecretNumber();
+        $gameId = $model->createId();
+
+        while (!$isFinished) {
+            $userData = readline("Ваше число (или 'exit' для выхода): ");
+
+            $triesCounter++;
+
+            $counter = 0;
+            $hints = [];
+
+            if ($userData === 'exit') {
+                $model->storeResult($username, $secretNumber, 0);
+                $model->storeTry($gameId, $triesCounter, $userData, 'Проигрыш');
+
+                $model->closeConnection();
+
+                View::showLose($secretNumber);
+                die();
+            } elseif (strlen($userData) === strlen($secretNumber)) {
+                for ($i = 0; $i < strlen($secretNumber); $i++) {
+                    if (mb_strpos($secretNumber, $userData[$i]) !== false) {
+                        if (mb_strpos($secretNumber, $userData[$i]) === $i) {
+                            $hints[] = 'Горячо';
+                        } else {
+                            $hints[] = 'Тепло';
+                        }
+                    } else {
+                        $hints[] = 'Холодно';
+                    }
+                }
+
+                sort($hints);
+
+                foreach ($hints as $status) {
+                    if ($status === 'Горячо') {
+                        $counter++;
+                    }
+                }
+
+                if ($counter === 3) {
+                    $isFinished = true;
+
+                    $model->storeResult($username, $secretNumber, 1);
+                    View::showWin($secretNumber);
+                }
+
+                View::showHints(implode(', ', $hints));
+
+                $isFinished ? $model->storeTry($gameId, $triesCounter, $userData, 'Победа') : $model->storeTry($gameId, $triesCounter, $userData, implode(', ', $hints));
+            }
+        }
+    }
+
+    public function showHistory()
+    {
+        $model = new Model('cold-hot.db');
+
+        $games = $model->getGames();
+
+        View::showHistory($games);
+    }
+
+    public function showGameRepeat()
+    {
+        $gameId = readline("Введите id сохраненной партии: ");
+
+        $model = new Model('cold-hot.db');
+
+        $game = $model->getGame($gameId);
+
+        View::showGameRepeat($game);
+    }
 }
